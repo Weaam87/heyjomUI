eb366d6c4775c81562087df67f6265a704a1ea93
Weaam alagha <32608267+Weaam87@users.noreply.github.com>
2024-03-08 16:57:19 +0800

    Add Retrofit dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
     implementation ("androidx.activity:activity-compose:1.8.2")
     implementation ("io.coil-kt:coil-compose:1.4.0")
     testImplementation("junit:junit:4.13.2")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
